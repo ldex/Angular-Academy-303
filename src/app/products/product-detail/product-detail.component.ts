1e85ca5d1ba64a9d2aa828449723db2bc5d9b03d
ldex <laurent.duveau+git@gmail.com>
2022-05-11 13:48:02 -0400

    product details route

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product.interface';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -12,14 +13,23 @@ export class ProductDetailComponent implements OnInit {
   @Input() product: Product;
 
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService
   ) {
 
    }
 
   ngOnInit(): void {
+    // Get the id parameter from the url
     let id = this.activatedRoute.snapshot.params.id;
 
+    // Get the product from the service
+    this
+      .productService
+      .getProductById(id)
+      .subscribe(
+        result => this.product = result
+      )
 
   }
 
